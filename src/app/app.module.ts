c21fb0eef3cdbbbbdf189124b52321ab891c828c
namnv <namnv@harmony-at.vn>
2023-09-28 08:17:24 +0700

    change useClass to HashLocationStrategy
    add useHash to false

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -228,7 +228,7 @@ function initializeKeycloak(keycloak: KeycloakService) {
           multi: true,
           deps: [KeycloakService]
         },
-        {provide: LocationStrategy, useClass: PathLocationStrategy}
+        {provide: LocationStrategy, useClass: HashLocationStrategy}
     ],
     bootstrap: [AppComponent]
 })
