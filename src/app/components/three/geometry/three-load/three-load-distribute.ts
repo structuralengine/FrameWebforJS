10abaa8116779d4dd148be03f4267b10487a7227
hanm <hanm@harmony-at.vn>
2024-09-23 13:47:01 +0700

    addition fix

--- src/app/components/three/geometry/three-load/three-load-distribute.ts
+++ src/app/components/three/geometry/three-load/three-load-distribute.ts
@@ -292,7 +292,11 @@ export class ThreeLoadDistribute {
     key: string
   ): void {
     for (const item of group.children) {
-      item.position.y = offset- 1;
+      if(group["L"]=== 0.8) {
+        item.position.y = offset- 1;
+      }else{
+        item.position.y = offset;
+      }
       // item.position.y = 0
     }
     group['offset'] = offset;
