8c7c5989d8f5d147c282a26212b59d9f90048608
DoanTrongBinh <binhdt@harmony-at.vn>
2024-11-25 09:20:06 +0700

    translate message in popup when leaving site

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -440,7 +440,8 @@ export class MenuComponent implements OnInit {
     if (this.electronService.isElectron) {
       this.electronService.ipcRenderer.send(IPC_MESSAGES.LOGIN);
     } else {
-      if (!this.loginDisplay && confirm('Your work will be lost. Do you want to leave this site?', )) {
+      const message = this.translate.instant("menu.leave")
+      if (!this.loginDisplay && confirm(message)) {
         this.msalBroadcastService.inProgress$
         .pipe(
           filter(
