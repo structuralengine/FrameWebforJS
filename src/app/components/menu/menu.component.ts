ba85a4490476d1de0047376571b9134433eecd6e
malme-khanhtrang <khanhtrang@malme.co.jp>
2025-05-19 22:12:32 +0700

    remove console

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -755,7 +755,6 @@ export class MenuComponent implements OnInit {
 
   handelClickChat() {
     const elementChat = document.getElementById("chatplusheader");
-    console.log("elementChat", elementChat)
     elementChat.click()
   }
 
